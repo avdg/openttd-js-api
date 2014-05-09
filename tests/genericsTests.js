48962a7f7536484016bbb3934baf68f64142fc6c
Anthony Van de Gejuchte <anthonyvdgent@yahoo.com>
2014-05-09 22:59:04 +0200

    Add tests for send/receive type tables

--- tests/genericsTests.js
+++ tests/genericsTests.js
@@ -140,4 +140,278 @@ describe('Generics (internals)', function() {
 			});
 		});
 	});
+
+	describe('Networking', function () {
+		describe('Type Table', function () {
+			describe('Notes about type testing:', function() {
+				it('- Make sure to test byte ordering', function() {});
+				it('- Make sure to test special values', function() {});
+				it('- Make sure to test (or let it fail) for cases of reading/writing outside the expected buffer',
+					function() {}
+				);
+			});
+
+			describe('Send', function () {
+				it('Should be able to create UInt8 buffers', function() {
+					var buffer = generics.typeTableSend['uint8'](8);
+
+					assert.equal(buffer.length, 1);
+					assert.deepEqual(buffer.toJSON(), [8]);
+				});
+
+				it('Should be able to create Int8 buffers', function() {
+					var buffer = generics.typeTableSend['int8'](-8);
+
+					assert.equal(buffer.length, 1);
+					assert.deepEqual(buffer.toJSON(), [248 /* 0xF8 */]);
+				});
+
+				it('Should be able to create UInt16LE buffers', function() {
+					var buffer = generics.typeTableSend['uint16le'](0x7877);
+
+					assert.equal(buffer.length, 2);
+					assert.deepEqual(buffer.toJSON(), [0x77, 0x78]);
+				});
+
+				it('Should be able to create UInt16BE buffers', function() {
+					var buffer = generics.typeTableSend['uint16be'](0x7877);
+
+					assert.equal(buffer.length, 2);
+					assert.deepEqual(buffer.toJSON(), [0x78, 0x77]);
+				});
+
+				it('Should be able to create Int16LE buffers', function() {
+					var buffer = generics.typeTableSend['int16le'](-0x77);
+
+					assert.equal(buffer.length, 2);
+					assert.deepEqual(buffer.toJSON(), [0x89, 0xff]);
+				});
+
+				it('Should be able to create Int16BE buffers', function() {
+					var buffer = generics.typeTableSend['int16be'](-0x77);
+
+					assert.equal(buffer.length, 2);
+					assert.deepEqual(buffer.toJSON(), [0xff, 0x89]);
+				});
+
+				it('Should be able to create UInt32LE buffers', function() {
+					var buffer = generics.typeTableSend['uint32le'](0x77767574);
+
+					assert.equal(buffer.length, 4);
+					assert.deepEqual(buffer.toJSON(), [0x74, 0x75, 0x76, 0x77]);
+				});
+
+				it('Should be able to create UInt32BE buffers', function() {
+					var buffer = generics.typeTableSend['uint32be'](0x77767574);
+
+					assert.equal(buffer.length, 4);
+					assert.deepEqual(buffer.toJSON(), [0x77, 0x76, 0x75, 0x74]);
+				});
+
+				it('Should be able to create Int32LE buffers', function() {
+					var buffer = generics.typeTableSend['int32le'](-0x777675);
+
+					assert.equal(buffer.length, 4);
+					assert.deepEqual(buffer.toJSON(), [0x8b, 0x89, 0x88, 0xff]);
+				});
+
+				it('Should be able to create Int32BE buffers', function() {
+					var buffer = generics.typeTableSend['int32be'](-0x777675);
+
+					assert.equal(buffer.length, 4);
+					assert.deepEqual(buffer.toJSON(), [0xff, 0x88, 0x89, 0x8b]);
+				});
+
+				it('Should be able to create floatLE buffers', function() {
+					var buffer = generics.typeTableSend['floatle'](.3);
+
+					assert.equal(buffer.length, 4);
+					assert.equal(buffer.readFloatLE(0) > .299, true);
+					assert.equal(buffer.readFloatLE(0) < .301, true);
+				});
+
+				it('Should be able to create floatBE buffers',  function() {
+					var buffer = generics.typeTableSend['floatbe'](.3);
+
+					assert.equal(buffer.length, 4);
+					assert.equal(buffer.readFloatBE(0) > .299, true);
+					assert.equal(buffer.readFloatBE(0) < .301, true);
+				});
+
+				it('Should be able to create doubleLE buffers', function() {
+					var buffer = generics.typeTableSend['doublele'](.3);
+
+					assert.equal(buffer.length, 8);
+					assert.equal(buffer.readDoubleLE(0) > .29999, true);
+					assert.equal(buffer.readDoubleLE(0) < .30001, true);
+				});
+
+				it('Should be able to create doubleBE buffers', function() {
+					var buffer = generics.typeTableSend['doublebe'](.3);
+
+					assert.equal(buffer.length, 8);
+					assert.equal(buffer.readDoubleBE(0) > .29999, true);
+					assert.equal(buffer.readDoubleBE(0) < .30001, true);
+				});
+
+				it('Should be able to create bool buffers', function() {
+					var buffer = generics.typeTableSend['bool'](true);
+
+					assert.equal(buffer.length, 1);
+					assert.deepEqual(buffer.toJSON(), [0x01]);
+				});
+
+				it('Should be able to create utf8 buffers', function() {
+					var buffer = generics.typeTableSend['utf8']('Test');
+
+					assert.equal(buffer.length, 5);
+					assert.deepEqual(buffer.toJSON(), [0x54, 0x65, 0x73, 0x74, 0x00]);
+				});
+
+				it('Should not be able to create malicious utf8 buffers containing null chars', function() {
+					var buffer = generics.typeTableSend['utf8']('\u0000Te\u0000st');
+
+					assert.equal(buffer.length, 5);
+					assert.deepEqual(buffer.toJSON(), [0x54, 0x65, 0x73, 0x74, 0x00]);
+				});
+			});
+
+			describe('Receive', function () {
+				it ('Note: returns an array of 2 elements with format [output, buffer length]',
+					function() {}
+				);
+
+				it ('Should be able to read boolean buffers', function() {
+					var output = generics.typeTableReceive['bool'](new Buffer([0x00], 'hex'), 0);
+
+					assert.deepEqual(output, [false, 1]);
+				});
+
+				it('Should be able to read UInt8 buffers', function() {
+					var output = generics.typeTableReceive['uint8'](new Buffer([0x88], 'hex'), 0);
+
+					assert.deepEqual(output, [0x88, 1]);
+				});
+
+				it('Should be able to read Int8 buffers', function() {
+					var output = generics.typeTableReceive['int8'](new Buffer([0x88], 'hex'), 0);
+
+					assert.deepEqual(output, [-0x78, 1]);
+				});
+
+				it('Should be able to read UInt16BE buffers', function() {
+					var output = generics.typeTableReceive['uint16be'](new Buffer([0x88, 0x87]), 0);
+
+					assert.deepEqual(output, [0x8887, 2]);
+				});
+
+				it('Should be able to read UInt16LE buffers', function() {
+					var output = generics.typeTableReceive['uint16le'](new Buffer([0x88, 0x87]), 0);
+
+					assert.deepEqual(output, [0x8788, 2]);
+				});
+
+				it('Should be able to read Int16BE buffers', function() {
+					var output = generics.typeTableReceive['int16be'](new Buffer([0x88, 0x87]), 0);
+
+					assert.deepEqual(output, [-0x7779, 2]);
+				});
+
+				it('Should be able to read Int16LE buffers', function() {
+					var output = generics.typeTableReceive['int16le'](new Buffer([0x88, 0x87]), 0);
+
+					assert.deepEqual(output, [-0x7878, 2]);
+				});
+
+				it('Should be able to read UInt32BE buffers', function() {
+					var output = generics.typeTableReceive['uint32be'](new Buffer([0x88, 0x87, 0x86, 0x85]), 0);
+
+					assert.deepEqual(output, [0x88878685, 4]);
+				});
+
+				it('Should be able to read UInt32LE buffers', function() {
+					var output = generics.typeTableReceive['uint32le'](new Buffer([0x88, 0x87, 0x86, 0x85]), 0);
+
+					assert.deepEqual(output, [0x85868788, 4]);
+				});
+
+				it('Should be able to read Int32BE buffers', function() {
+					var output = generics.typeTableReceive['int32be'](new Buffer([0x88, 0x87, 0x86, 0x85]), 0);
+
+					assert.deepEqual(output, [-0x7778797b, 4]);
+				});
+
+				it('Should be able to read Int32LE buffers', function() {
+					var output = generics.typeTableReceive['int32le'](new Buffer([0x88, 0x87, 0x86, 0x85]), 0);
+
+					assert.deepEqual(output, [-0x7a797878, 4]);
+				});
+
+				it('Should be able to read floatBE buffers', function() {
+					var buffer = new Buffer(4);
+					buffer.writeFloatBE(0.3, 0);
+
+					var output = generics.typeTableReceive['floatbe'](buffer, 0);
+
+					assert.equal(output[0] > 0.299, true);
+					assert.equal(output[0] < 0.301, true);
+					assert.equal(output[1], 4);
+				});
+
+				it('Should be able to read floatLE buffers', function() {
+					var buffer = new Buffer(4);
+					buffer.writeFloatLE(0.3, 0);
+
+					var output = generics.typeTableReceive['floatle'](buffer, 0);
+
+					assert.equal(output[0] > 0.299, true);
+					assert.equal(output[0] < 0.301, true);
+					assert.equal(output[1], 4);
+				});
+
+				it('Should be able to read doubleBE buffers', function() {
+					var buffer = new Buffer(8);
+					buffer.writeDoubleBE(0.3, 0);
+
+					var output = generics.typeTableReceive['doublebe'](buffer, 0);
+
+					assert.equal(output[0] > 0.29999, true);
+					assert.equal(output[0] < 0.30001, true);
+					assert.equal(output[1], 8);
+				});
+
+				it('Should be able to read doubleLE buffers', function() {
+					var buffer = new Buffer(8);
+					buffer.writeDoubleLE(0.3, 0);
+
+					var output = generics.typeTableReceive['doublele'](buffer, 0);
+
+					assert.equal(output[0] > 0.29999, true);
+					assert.equal(output[0] < 0.30001, true);
+					assert.equal(output[1], 8);
+				});
+
+				it('Should be able to read utf8 buffers', function() {
+					var output = generics.typeTableReceive['utf8'](new Buffer('Test\u0000'), 0);
+
+					assert.deepEqual(output, ['Test', 5]);
+				});
+
+				it('Should be able to read empty utf8 buffers', function() {
+					var output = generics.typeTableReceive['utf8'](new Buffer([0x00]), 0);
+
+					assert.deepEqual(output, ['', 1]);
+				});
+
+				it('Should be able to read UInt64BE buffers');
+				it('Should be able to read UInt64LE buffers');
+				it('Should be able to read Int64BE buffers');
+				it('Should be able to read Int64LE buffers');
+
+				// TODO There are no plans for these buffers if they might stay or not
+				it('Should be able to read protocol buffers');
+				it('Should be able to read cmdnames buffers');
+			});
+		});
+	});
 });
